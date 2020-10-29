fd7b522b1a99ac4a755ea0743f1c51bd47e37fed
ingnolasco <70291950+ingnolasco@users.noreply.github.com>
2020-10-29 15:11:24 -0600

    encrip
    enc

---
+++ EncriptadorPalabras/EncriptadorPalabras/Clases/Encriptador.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncriptadorPalabras.Clases
+{
+   public class Encriptador
+    {
+
+        // Función para cifrar una cadena.
+        public string Cifrar(string cadena)
+        {
+            byte[] llave = UTF32Encoding.UTF8.GetBytes("prueba"); //Arreglo donde guardaremos la llave para el cifrado 3DES.
+            byte[] arreglo = UTF8Encoding.UTF8.GetBytes(cadena); //Arreglo donde guardaremos la cadena descifrada.
+                                                                 // Ciframos utilizando el Algoritmo MD5.
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            llave = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes("cadenadecifrado"));
+            md5.Clear();
+            //Ciframos utilizando el Algoritmo 3DES.
+            TripleDESCryptoServiceProvider tripledes = new TripleDESCryptoServiceProvider
+            {
+                Key = llave,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            ICryptoTransform convertir = tripledes.CreateEncryptor(); // Iniciamos la conversión de la cadena
+            byte[] resultado = convertir.TransformFinalBlock(arreglo, 0, arreglo.Length); //Arreglo de bytes donde guardaremos la cadena cifrada.
+            tripledes.Clear();
+            return Convert.ToBase64String(resultado, 0, resultado.Length); // Convertimos la cadena y la regresamos.
+        }
+
+
+        // Función para descifrar una cadena.
+        public string Descifrar(string cadena)
+        {
+            byte[] llave = UTF32Encoding.UTF8.GetBytes("prueba");
+            byte[] arreglo = Convert.FromBase64String(cadena); // Arreglo donde guardaremos la cadena descovertida.
+            // Ciframos utilizando el Algoritmo MD5.
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            llave = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes("cadenadecifrado"));
+            md5.Clear();
+            //Ciframos utilizando el Algoritmo 3DES.
+            TripleDESCryptoServiceProvider tripledes = new TripleDESCryptoServiceProvider
+            {
+                Key = llave,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform convertir = tripledes.CreateDecryptor();
+            byte[] resultado = convertir.TransformFinalBlock(arreglo, 0, arreglo.Length);
+            tripledes.Clear();
+            string cadena_descifrada = UTF8Encoding.UTF8.GetString(resultado); // Obtenemos la cadena
+            return cadena_descifrada; // Devolvemos la cadena
+        }
+
+    }
+}
